53c29f4f0b2a7a2ff0654be76ffd38eb10ceaffc
rqlalvs <raquelalves3753@gmail.com>
2023-06-21 14:38:34 -0300

    Quarta aula: Criando diretorio e utilizando IF

--- jpg-to-png-converter.sh
+++ jpg-to-png-converter.sh
@@ -2,7 +2,13 @@
 
 cd ~/Downloads/imagens-livros
 
+if [ ! -d png ]
+then
+    mkdir png
+fi
+
 for imagem in *.jpg
 do
-    convert $imagem $imagem.png
+    imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
+    convert $imagem png/$imagem_sem_extensao.png
 done
