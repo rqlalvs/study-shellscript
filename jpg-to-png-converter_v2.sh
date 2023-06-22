bb2736698c67d744485c77729af96353eb527df6
rqlalvs <raquelalves3753@gmail.com>
2023-06-22 10:56:00 -0300

    Setima aula: Funcao recursiva e varredura de diretorio

--- jpg-to-png-converter_v2.sh
+++ jpg-to-png-converter_v2.sh
@@ -2,21 +2,24 @@
 
 cd ~/Downloads/imagens-novos-livros
 
-for arquivo in *
-do
-    if [ -d $arquivo ]
-    then
-        cd $arquivo
-        for conteudo_arquivo in *
-        do
-            if [ -d $conteudo_arquivo ]
-            then
-                #entra e repete
-            else
-                #Converte aqui tbm
-            fi
-        done
-    else
-        #converte aqui
-    fi
-done
+converte_imagem(){
+    local caminho_imagem=$1
+    local imagem_sem_extensao=$(ls $caminho_imagem | awk -F. '{ print $1 }')
+    convert $imagem_sem_extensao.jpg $imagem_sem_extensao.png
+}
+
+varrer_diretorio(){
+    cd $1
+    for arquivo in *
+    do
+        local caminho_arquivo=$(find ~/Downloads/imagens-novos-livros -name $arquivo)
+        if [ -d $caminho_arquivo ]
+        then
+            varrer_diretorio $caminho_arquivo
+        else
+            converte_imagem $caminho_arquivo
+        fi
+    done
+}
+
+varrer_diretorio ~/Downloads/imagens-novos-livros
