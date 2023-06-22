84c555b471367b5f6f49b3990dc65c178c04e1b2
rqlalvs <raquelalves3753@gmail.com>
2023-06-22 12:15:37 -0300

    Ultima aula: Script logando data, nome e peso dos 10 processos em MB

--- log-process-top10.sh
+++ log-process-top10.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+if [ ! -d log ]
+then
+    mkdir log
+fi
+
+processos(){
 processos=$(ps -e -o pid --sort -size | head -n 11 | grep [0-9])
 for pid in $processos
 do
-    nome_processo = $(ps -p $pid -o comm=)
-    echo $(date +%F,%H:%M:%S) >> $nome_processo.log
+    nome_processo=$(ps -p $pid -o comm=)
+    echo $(date +%F,%H:%M:%S) >> log/$nome_processo.log
+    tamanho_processo=$(ps -p $pid -o size| grep [0-9])
+    echo "$(bc <<< "scale=2;$tamanho_processo/1024") MB" >>> log/$nome_processo.log
 done
+}
+
+processos
+if [ $? -eq 0]
+then
+    echo "Sucesso."
+else
+    echo "Houve algum problema"
+fi
+
