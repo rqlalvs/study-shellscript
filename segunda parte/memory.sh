53e6379ac9a2d921be2ce4218f50f6a8b9f81429
rqlalvs <raquelalves3753@gmail.com>
2023-06-28 17:03:05 -0300

    Setima aula: usando free -h

--- segunda parte/memory.sh
+++ segunda parte/memory.sh
@@ -3,3 +3,9 @@
 memoria_total=$(free | grep -i mem | awk '{print $2}')
 memoria_consumida=$(free | grep -i mem | awk '{print $3}') 
 relacao_memoria=$(bc <<< "scale=2;memoria_total/memoria_consumida *100" | awk -F. '{print $1}')
+
+if [ $relacao_memoria -gt 50]
+then
+mail -s "Consumo de memoria acida do limite" adm.multillidae@gmail.com<<del
+O consumo de memoria esta acima do limite. Atualmente o consumo e relacao_memoria
+del
